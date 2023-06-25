401f0ce63072c46b3b88d08faa17cbc57eb67097
danielekstrom <danielekstrom@proxmox-deb.prox-deb.local>
2023-06-25 22:38:16 +0200

    first commit

---
+++ app/bot.py
@@ -0,0 +1,54 @@
+import discord
+import responses
+import requests
+from discord.ext import tasks
+import os
+from dotenv import load_dotenv
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    print("STARTING")
+    load_dotenv()
+    TOKEN = os.getenv('TOKEN')
+    APIKEY = os.getenv('APIKEY')
+    ChannelID = int(os.getenv('ChannelID'))
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running')
+        checkCurrMap.start()
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+        print(f'{username} said: {user_message} in {channel}')
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    @tasks.loop(minutes=1)
+    async def checkCurrMap():
+        channel = client.get_channel(ChannelID)
+        response = requests.get(
+            f"https://api.mozambiquehe.re/maprotation?auth={APIKEY}&version=2").json()
+        map = response["ranked"]["current"]["map"]
+        await channel.edit(name=map)
+
+    client.run(TOKEN)
---
+++ app/responses.py
@@ -0,0 +1,57 @@
+import random
+import requests
+import os
+
+
+def handle_response(message) -> str:
+    APIKEY = os.getenv('APIKEY')
+    p_message = message.lower()
+
+    if p_message == 'hello':
+        return 'Hello!'
+
+    if p_message == 'roll':
+        return str(random.randint(1, 6))
+
+    if p_message == '!help':
+        return "`!map - Check the current map and the Next map!` \n`!crafting - Check whats in the replicator!`"
+
+    if p_message == '!crafting':
+        return formatCraftingResponse(APIKEY)
+
+    if p_message == '!map':
+        return formatMapResponse(APIKEY)
+
+
+def formatMapResponse(APIKEY) -> str:
+    result = ""
+    response = requests.get(
+        f"https://api.mozambiquehe.re/maprotation?auth={APIKEY}&version=2").json()
+    for i in response["ranked"]:
+        map = response["ranked"][i]["map"]
+        started = list(response["ranked"][i]["readableDate_start"][len(
+            response["ranked"][i]["readableDate_start"]) - 8:])
+        started[1] = str(int(started[1]) + 2)
+
+        ends = list(response["ranked"][i]["readableDate_end"][len(
+            response["ranked"][i]["readableDate_end"]) - 8:])
+        ends[1] = str(int(ends[1]) + 2)
+
+        result += f'{i.capitalize()} Map: {map} \nStarted: {"".join(started)} \nEnds: {"".join(ends)} \n\n'
+    return result
+
+
+def formatCraftingResponse(APIKEY) -> str:
+    results = ""
+    checked = True
+    response = requests.get(
+        "https://api.mozambiquehe.re/crafting?auth={APIKEY}").json()
+    for i in response:
+        if checked:
+            results += f'↓ {i["bundleType"].capitalize()} ↓ \n'
+            if (i["bundleType"] == "permanent"):
+                checked = False
+        for j in i["bundleContent"]:
+            if j["itemType"]["name"] != "ammo":
+                results += f'{j["itemType"]["name"].capitalize().replace("_", " ")} \n'
+    return results
